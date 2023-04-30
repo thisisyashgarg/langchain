9e0ec2fd8fd1a3a53ed148da7ef8569c60904e30
Yash Garg <thisisyashgarg@gmail.com>
2023-04-30 15:06:56 +0530

    added wikipedia agent to search through wikipedia and create a story

--- app.py
+++ app.py
@@ -5,41 +5,57 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = apiKey
 
-# prompt  template
+# Prompt Template
 cast_template = PromptTemplate(
     input_variables=['title'],
     template="Write a list of characters with their persona for a story whose title is : {title}"
 )
 story_template = PromptTemplate(
-    input_variables=['cast'],
-    template="Write a story according to the cast which are {cast}"
+    input_variables=['cast', 'wikipedia_research'],
+    template="Write a story according to the cast which are {cast} while leveraging the wikipedia research as well : {wikipedia_research}"
 )
 
 # Memory
-memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
+cast_memory = ConversationBufferMemory(
+    input_key='title', memory_key='chat_history')
+story_memory = ConversationBufferMemory(
+    input_key='cast', memory_key='chat_history')
 
-# App
+# App Structure
 st.title('My AutoGPT')
 prompt = st.text_input(
-    "Enter the title of the story and I will give you the story")
+    "Enter a story title")
 
 # LLM
 llm = OpenAI(temperature=0.75)
 cast_chain = LLMChain(llm=llm, prompt=cast_template,
-                      verbose=True, output_key='cast', memory=memory)
+                      verbose=True, output_key='cast', memory=cast_memory)
 story_chain = LLMChain(llm=llm, prompt=story_template,
-                       verbose=True, output_key='story', memory=memory)
-sequential_chain = SequentialChain(
-    chains=[cast_chain, story_chain], input_variables=['title'], output_variables=['cast', 'story'], verbose=True)
+                       verbose=True, output_key='story', memory=story_memory)
+wiki = WikipediaAPIWrapper()
+# sequential_chain = SequentialChain(
+#     chains=[cast_chain, story_chain], input_variables=['title'], output_variables=['cast', 'story'], verbose=True)
 
-# response
+# Response from the AI
 if prompt:
-    response = sequential_chain({"title": prompt})
-    st.write(response['cast'])
-    st.write(response['story'])
+    # response = sequential_chain({"title": prompt})
+    cast = cast_chain.run(prompt)
+    wiki_research = wiki.run(cast)
+    story = story_chain.run(cast=cast, wikipedia_research=wiki_research)
 
-    with st.expander("Message History"):
-        st.info(memory.buffer)
+    st.write(cast)
+    st.write(story)
+
+    # History Chats
+    with st.expander("Cast History"):
+        st.info(cast_memory.buffer)
+
+    with st.expander("Story History"):
+        st.info(story_memory.buffer)
+
+    with st.expander("Wikipedia History"):
+        st.info(wiki_research)
